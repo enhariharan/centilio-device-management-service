cbc7eeaeaad66874ca041c17b3d6fedd03e20a79
Hariharan Narayanan <enhariharan@gmail.com>
2016-12-27 18:04:45 +0530

    added page-testing support

--- centilio_device_management_service.js
+++ centilio_device_management_service.js
@@ -3,19 +3,31 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+// Detect if pageload url has test=1 in query.  If so, enable pagetest mode.
+app.use(function(req, res, next) {
+  console.log('1');
+  res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+  next();
+});
 
 // Handle / route. NOTE: This method SHOULD stay above 404 handler method
 app.get('/', function(req, res){
+  console.log('2');
   res.render('home');
 });
 
 // Handle /about route. NOTE: This method SHOULD stay above 404 handler method
 app.get('/about', function(req, res){
-  res.render('about');
+  console.log('3');
+  res.render('about', { pageTestScript: '/qa/tests-about.js' });
 });
 
 // cuatom 404 page
 app.use(function(req, res) {
+  console.log('4');
   console.error('404 - Page not found');
   res.status(404);
   res.render('404');
@@ -23,6 +35,7 @@ app.use(function(req, res) {
 
 // custom 500 page
 app.use(function(err, req, res, next) {
+  console.log('5');
   console.error(err.stack);
   res.status(500);
   res.render('500');
