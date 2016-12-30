80961fa1acb54348204f4e9069c41ce57dd85996
Hariharan Narayanan <enhariharan@gmail.com>
2016-12-30 17:39:35 +0530

    Added a new Mongodb model for client

---
+++ models/client-model.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Address = require('../address.js');
+
+var clientModel = {
+  // id and timestamp
+  uuid: String,
+  timestamp: Date,
+
+  // corporate name or the retail the person who is signing up
+  name: String,
+
+  // Client type. "corporate", "retail", etc.,
+  type: String,
+
+  // Addresses of the client
+  addresses: [Address],
+};
+
+var clientSchema = mongoose.Schema(clientModel);
+
+var Client = mongoose.model('Client', clientSchema);
+module.exports = Client;
