fff97da82cf5db4ae68d9f10c6be8e6772d3e00d
Hariharan Narayanan <enhariharan@gmail.com>
2017-01-01 08:05:29 +0530

    Updated apidocs

--- doc/apidoc/api_project.js
+++ doc/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-12-31T07:28:56.771Z",
+    "time": "2017-01-01T02:34:49.553Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
