434ecf51513614cec281c333162dc1ac2a27affb
Hariharan Narayanan <enhariharan@gmail.com>
2017-01-04 15:42:53 +0530

    updated documentation

--- controllers/clients-controller.js
+++ controllers/clients-controller.js
@@ -138,7 +138,7 @@ exports.getAllClients = function (req, res) {
  *     }
  *
  * @apiError (400) {String} BadRequest Error code 400 is returned if the JSON format is incorrect.
- * @apiError (500) {String} InternalServerError Error code 500 is returned in case of osme error in the server.
+ * @apiError (500) {String} InternalServerError Error code 500 is returned in case of some error in the server.
  */
 exports.addClient = function (req, res) {
   if (!req || !req.body) {
