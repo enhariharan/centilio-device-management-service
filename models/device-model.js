983c6e83dcc51c2b7488ac95e3c17f2006a902d3
Hariharan Narayanan <enhariharan@gmail.com>
2017-01-05 14:09:49 +0530

    integrated device-model with device-type-model

--- models/device-model.js
+++ models/device-model.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose'),
+    DeviceType = require('./device-type-model.js'),
     Schema = mongoose.Schema;
 
 var deviceSchema = Schema({
   // unique id and created timestamp
-  uuid: String,
-  timestamp: Date,
+  uuid: {type: String, required: true},
+  timestamp: {type: Date, required: true},
 
   // name of the device
   name: String,
@@ -14,7 +15,10 @@ var deviceSchema = Schema({
   longitude: String,
 
   // current status of the device - "new", "registered", "activated", "online", "offline", "retired"
-  status: String,
+  status: {type: String, required: true},
+
+  // device type. stores uuid of device type which must be aleady present in "devicetypes" collection.
+  deviceType: {type: String, ref: 'DeviceType'},
 });
 
 var Device = mongoose.model('Device', deviceSchema);
