a1d699f50c57d94719243f69b0de0fcf717a2888
Hariharan Narayanan <enhariharan@gmail.com>
2017-01-31 08:49:17 +0530

    added module src/app/push-notifications.js

---
+++ src/app/push-notifications.js
@@ -0,0 +1,27 @@
+var socketio = require('socket.io'),
+    webSocketIO = null,
+    features = require('./features');
+var socket = null;
+
+var startWebDocketServer = (webServer) => {
+  "use strict";
+
+  if (features.pushNotifications !== true) {return;}
+
+  webSocketIO = socketio(webServer);
+  console.log('push notificatios enabled ');
+  webSocketIO.on('connection', (skt) => {
+    socket = skt;
+    console.log('A new client connected: ' + JSON.stringify(socket));
+    // setTimeout(() => {socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});}, 4000);
+    socket.on('disconnect', () => {console.log('A client disconnected');});
+  });
+};
+
+var sendDeviceReadingNotification = (readings) => {
+  "use strict";
+  if (features.pushNotifications !== true) {return;}
+  if (socket) socket.emit('readingsNotification', {description: 'A device reading is now available'});
+};
+
+module.exports = {startWebDocketServer, sendDeviceReadingNotification};
