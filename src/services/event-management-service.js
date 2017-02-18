104985442fa9cc2a1ec8abced1cadee07b1a7985
Hariharan Narayanan <enhariharan@gmail.com>
2017-02-18 22:21:21 +0530

    added logs to sendEventNotification()

--- src/services/event-management-service.js
+++ src/services/event-management-service.js
@@ -4,18 +4,23 @@ exports.sendEventNotification = (event) => {
   "use strict";
   return new Promise() {
     (resolve, reject) => {
-      switch (event.name) {
+        console.log('\nsendEventNotification() - event: ' + JSON.stringify(event));
+        console.log('\nevent.name: ' + JSON.stringify(event.name));
+        switch (event.name) {
         case 'displayBrightness':
+          console.log('\ndisplayBrightness'); 
           PushNotifications.sendDisplayBrightnessNotification(event);
           resolve(event);
           break;
 
         case 'playAudio':
+          console.log('\nplayAudio'); 
           PushNotifications.sendPlayAudioNotification(event);
           resolve(event);
           break;
 
         default:
+          console.log('\nerror'); 
           reject(400); // sending 400 - bad request error for an unknown event
           break;
       }
